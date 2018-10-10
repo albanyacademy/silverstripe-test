fed182d3e18c2c35b35f9a2d7beecc5445064eb4
clindsay <cameron.lindsay@esentire.com>
2018-10-10 09:54:23 -0400

    updating gitignore

---
+++ tets.php
@@ -0,0 +1,11 @@
+<?php
+function base64_encode_image ($filename=string,$filetype=string) {
+    if ($filename) {
+        $imgbinary = fread(fopen($filename, "r"), filesize($filename));
+        return 'data:image/' . $filetype . ';base64,' . base64_encode($imgbinary);
+    }
+}
+?>
+
+
+<img src="<?php echo base64_encode_image ('rsa-logo.png','png'); ?>"/>
